f0d97c4b6e616b2f5e8ed477232442c75c1c6623
Tom van Greevenbroek <tom.v.g@outlook.com>
2023-04-06 18:05:41 +0200

    initialize project

---
+++ src/main/java/com/detection/text/TextDetectionApplication.java
@@ -0,0 +1,13 @@
+package com.detection.text;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextDetectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TextDetectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/detection/text/TextDetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.detection.text;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextDetectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
